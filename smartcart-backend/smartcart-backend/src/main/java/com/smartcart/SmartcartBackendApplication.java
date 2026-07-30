ffa12251b84b197d9e7225894c4c4e5382255ad5
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2026-07-30 13:30:37 +0530

    Debug Render env

--- smartcart-backend/smartcart-backend/src/main/java/com/smartcart/SmartcartBackendApplication.java
+++ smartcart-backend/smartcart-backend/src/main/java/com/smartcart/SmartcartBackendApplication.java
@@ -11,10 +11,13 @@ public class SmartcartBackendApplication {
 
 	public static void main(String[] args) {
 
+		System.out.println("DB_URL exists = " + (System.getenv("DB_URL") != null));
+		System.out.println("DB_USERNAME exists = " + (System.getenv("DB_USERNAME") != null));
+		System.out.println("DB_PASSWORD exists = " + (System.getenv("DB_PASSWORD") != null));
+
 		SpringApplication.run(
 				SmartcartBackendApplication.class,
 				args
 		);
 	}
-
 }
