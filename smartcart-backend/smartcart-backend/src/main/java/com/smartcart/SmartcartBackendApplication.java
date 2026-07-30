4054c00caa642725cf9aa241ffb6c0bc5e74ba81
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2026-07-30 14:58:35 +0530

    Production cleanup

--- smartcart-backend/smartcart-backend/src/main/java/com/smartcart/SmartcartBackendApplication.java
+++ smartcart-backend/smartcart-backend/src/main/java/com/smartcart/SmartcartBackendApplication.java
@@ -4,20 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-
 @SpringBootApplication
 @EnableAsync
 public class SmartcartBackendApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("DB_URL exists = " + (System.getenv("DB_URL") != null));
-		System.out.println("DB_USERNAME exists = " + (System.getenv("DB_USERNAME") != null));
-		System.out.println("DB_PASSWORD exists = " + (System.getenv("DB_PASSWORD") != null));
-
-		SpringApplication.run(
-				SmartcartBackendApplication.class,
-				args
-		);
+		SpringApplication.run(SmartcartBackendApplication.class, args);
 	}
 }
